85111157b5373f11b7307089f9991778e7703064
Yohanan Baruchel <yohanan@yad2.co.il>
2020-01-04 18:21:40 +0200

    Remove vendor folder in gitignore

--- src/main.go
+++ src/main.go
@@ -18,11 +18,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Portuguese (BR) city name: %v\n", record.City.Names["pt-BR"])
+	fmt.Printf("Portuguese (BR) city name: %v\n", record.City.Names["he-IL"])
 	if len(record.Subdivisions) > 0 {
-		fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
+		fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["he"])
 	}
-	fmt.Printf("Russian country name: %v\n", record.Country.Names["ru"])
+	fmt.Printf("Russian country name: %v\n", record.Country.Names["he"])
 	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
 	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
 	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
