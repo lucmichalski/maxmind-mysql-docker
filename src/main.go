86a1227ec3fe3d3e44253efcd517f3802eefc5b5
derkinderfietsen <20744388+derkinderfietsen@users.noreply.github.com>
2020-10-05 23:33:09 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -22,7 +22,7 @@ func main() {
 	if len(record.Subdivisions) > 0 {
 		fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["he"])
 	}
-	fmt.Printf("Russian country name: %v\n", record.Country.Names["he"])
+	fmt.Printf("Country name: %v\n", record.Country.Names["he"])
 	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
 	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
 	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
